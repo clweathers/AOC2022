409c1cbe39dbb99c38c86486f2ea2792e6f49e8c
Chris Weathers <clweathers1@gmail.com>
2022-12-05 17:54:22 -0500

    Day 5 - initial commit.

---
+++ 5/5_test.rb
@@ -0,0 +1,24 @@
+require "test/unit"
+require_relative "5.rb"
+
+class FiveTest < Test::Unit::TestCase
+    # def test_a_sample
+    #     answer = _("sample.txt")
+    #     assert_equal(answer, 1)
+    # end
+
+    # def test_a_input
+    #     answer = _("input.txt")
+    #     assert_equal(answer, 1)
+    # end
+
+    # def test_b_sample
+    #     answer = _("sample.txt")
+    #     assert_equal(answer, 1)
+    # end
+
+    # def test_b_input
+    #     answer = _("input.txt")
+    #     assert_equal(answer, 1)
+    # end
+end
