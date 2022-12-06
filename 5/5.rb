443853d3608d3eead35e083a4a064d726c619bc1
Chris Weathers <clweathers1@gmail.com>
2022-12-05 23:14:59 -0500

    Day 5 - part one complete.

--- 5/5.rb
+++ 5/5.rb
@@ -1,3 +1,82 @@
+def stacks_and_moves(filename)
+    lines = File.readlines(filename)
+
+    line_length = lines[0].length
+    number_of_stacks = number_of_stacks_for_line_length(line_length)
+
+    stacks = Array.new(number_of_stacks) {[]}
+    moves = []
+
+    lines.each do |line|
+        if line.include? "["
+            for stack_index in 0...number_of_stacks do
+                stack = stacks[stack_index]
+                character_index = character_index_for_stack_index(stack_index)
+                crate = line[character_index]
+                stack.prepend(crate) if crate != " "
+            end
+        elsif line.include? "move"
+            matches = line.match(/move (?<number_of_boxes>\d+) from (?<start_stack_index>\d+) to (?<end_stack_index>\d+)/)
+            number_of_boxes = matches[:number_of_boxes].to_i
+            start_stack_index = matches[:start_stack_index].to_i - 1
+            end_stack_index = matches[:end_stack_index].to_i - 1
+            move = {number_of_boxes: number_of_boxes, start_stack_index: start_stack_index, end_stack_index: end_stack_index}
+            moves << move
+        end
+    end
+
+    return stacks, moves
+end
+
+def crates_on_top(stacks)
+    crates_on_top = stacks.map(&:last)
+    return crates_on_top
+end
+
+def rearranged_stacks(stacks, moves)
+    stacks = stacks.clone
+
+    moves.each do |move|
+        number_of_boxes = move[:number_of_boxes]
+        start_stack_index = move[:start_stack_index]
+        end_stack_index = move[:end_stack_index]
+
+        start_stack = stacks[start_stack_index]
+        end_stack = stacks[end_stack_index]
+
+        number_of_boxes.times do
+            crate = start_stack.pop
+            end_stack << crate
+        end
+    end
+
+    return stacks
+end
+
+def number_of_stacks_for_line_length(line_length)
+    number_of_stacks = (line_length + 1) / 4
+    return number_of_stacks
+end
+
+def character_index_for_stack_index(stack_index)
+    character_index = (stack_index * 4) + 1
+    return character_index
+end
+
+def rearranged_crates_on_top(filename)
+    stacks, moves = stacks_and_moves(filename)
+    rearranged_stacks = rearranged_stacks(stacks, moves)
+    rearranged_crates_on_top = crates_on_top(rearranged_stacks)
+    return rearranged_crates_on_top
+end
+
+def rearranged_crates_on_top_string(filename)
+    rearranged_crates_on_top = rearranged_crates_on_top(filename)
+    rearranged_crates_on_top_string = rearranged_crates_on_top.join
+    return rearranged_crates_on_top_string
+end
+
 if __FILE__ == $0
-    
+    rearranged_crates_on_top_string = rearranged_crates_on_top_string("input.txt")
+    puts rearranged_crates_on_top_string
 end
--- 5/5_test.rb
+++ 5/5_test.rb
@@ -2,15 +2,15 @@ require "test/unit"
 require_relative "5.rb"
 
 class FiveTest < Test::Unit::TestCase
-    # def test_a_sample
-    #     answer = _("sample.txt")
-    #     assert_equal(answer, 1)
-    # end
+    def test_a_sample
+        answer = rearranged_crates_on_top_string("sample.txt")
+        assert_equal(answer, "CMZ")
+    end
 
-    # def test_a_input
-    #     answer = _("input.txt")
-    #     assert_equal(answer, 1)
-    # end
+    def test_a_input
+        answer = rearranged_crates_on_top_string("input.txt")
+        assert_equal(answer, "RTGWZTHLD")
+    end
 
     # def test_b_sample
     #     answer = _("sample.txt")
