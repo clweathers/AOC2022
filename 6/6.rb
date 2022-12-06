93ec1ce429c6ced501880f82990a758351ad25bb
Chris Weathers <clweathers1@gmail.com>
2022-12-06 01:04:39 -0500

    Day 6 - part two complete.

--- 6/6.rb
+++ 6/6.rb
@@ -1,28 +1,28 @@
-def markers_in_string(string)
+def markers_in_string(string, marker_length)
     markers = []
 
-    string.chars.each_cons(4) do |characters|
+    string.chars.each_cons(marker_length) do |characters|
         markers << characters.join if characters.size == characters.uniq.size
     end
     
     return markers
 end
 
-def first_marker_in_string(string)
-    markers = markers_in_string(string)
+def first_marker_in_string(string, marker_length)
+    markers = markers_in_string(string, marker_length)
     marker = markers.first
     return marker
 end
 
-def end_index_of_marker_in_string(marker, string)
+def end_index_of_marker_in_string(marker, string, marker_length)
     start_index = string.index(marker)
-    end_index = start_index + marker.length
+    end_index = start_index + marker_length
     return end_index
 end
 
-def end_index_of_first_marker_in_string(string)
-    first_marker = first_marker_in_string(string)
-    end_index = end_index_of_marker_in_string(first_marker, string)
+def end_index_of_first_marker_in_string(string, marker_length)
+    first_marker = first_marker_in_string(string, marker_length)
+    end_index = end_index_of_marker_in_string(first_marker, string, marker_length)
     return end_index
 end
 
@@ -33,6 +33,7 @@ end
 
 if __FILE__ == $0
     string = string_from_file("input.txt")
-    end_index = end_index_of_first_marker_in_string(string)
+    marker_length = 14
+    end_index = end_index_of_first_marker_in_string(string, marker_length)
     puts end_index
 end
--- 6/6_test.rb
+++ 6/6_test.rb
@@ -2,34 +2,72 @@ require "test/unit"
 require_relative "6.rb"
 
 class SixTest < Test::Unit::TestCase
+
+    # Part one samples
+
     def test_a_sample
-        end_index = end_index_of_first_marker_in_string("mjqjpqmgbljsphdztnvjfqwrcgsmlb")
+        end_index = end_index_of_first_marker_in_string("mjqjpqmgbljsphdztnvjfqwrcgsmlb", 4)
         assert_equal(end_index, 7)
     end
 
     def test_b_sample
-        end_index = end_index_of_first_marker_in_string("bvwbjplbgvbhsrlpgdmjqwftvncz")
+        end_index = end_index_of_first_marker_in_string("bvwbjplbgvbhsrlpgdmjqwftvncz", 4)
         assert_equal(end_index, 5)
     end
 
     def test_c_sample
-        end_index = end_index_of_first_marker_in_string("nppdvjthqldpwncqszvftbrmjlhg")
+        end_index = end_index_of_first_marker_in_string("nppdvjthqldpwncqszvftbrmjlhg", 4)
         assert_equal(end_index, 6)
     end
 
     def test_d_sample
-        end_index = end_index_of_first_marker_in_string("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg")
+        end_index = end_index_of_first_marker_in_string("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg", 4)
         assert_equal(end_index, 10)
     end
 
     def test_e_sample
-        end_index = end_index_of_first_marker_in_string("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw")
+        end_index = end_index_of_first_marker_in_string("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw", 4)
         assert_equal(end_index, 11)
     end
 
-    def test_input
+    # Part two samples
+
+    def test_f_sample
+        end_index = end_index_of_first_marker_in_string("mjqjpqmgbljsphdztnvjfqwrcgsmlb", 14)
+        assert_equal(end_index, 19)
+    end
+
+    def test_g_sample
+        end_index = end_index_of_first_marker_in_string("bvwbjplbgvbhsrlpgdmjqwftvncz", 14)
+        assert_equal(end_index, 23)
+    end
+
+    def test_h_sample
+        end_index = end_index_of_first_marker_in_string("nppdvjthqldpwncqszvftbrmjlhg", 14)
+        assert_equal(end_index, 23)
+    end
+
+    def test_i_sample
+        end_index = end_index_of_first_marker_in_string("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg", 14)
+        assert_equal(end_index, 29)
+    end
+
+    def test_j_sample
+        end_index = end_index_of_first_marker_in_string("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw", 14)
+        assert_equal(end_index, 26)
+    end
+
+    # Inputs
+
+    def test_input_a
         string = string_from_file("input.txt")
-        end_index = end_index_of_first_marker_in_string(string)
+        end_index = end_index_of_first_marker_in_string(string, 4)
         assert_equal(end_index, 1578)
+    end
+
+    def test_input_b
+        string = string_from_file("input.txt")
+        end_index = end_index_of_first_marker_in_string(string, 14)
+        assert_equal(end_index, 2178)
     end
 end
