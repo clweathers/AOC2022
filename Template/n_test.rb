9fc04884481df27483c4a24293f73dad7e0f73a4
Chris Weathers <clweathers1@gmail.com>
2022-12-05 23:58:16 -0500

    Added template.

---
+++ Template/n_test.rb
@@ -0,0 +1,24 @@
+require "test/unit"
+require_relative "{number}.rb"
+
+class {number_string}Test < Test::Unit::TestCase
+    def test_a_sample
+        answer = _("sample.txt")
+        assert_equal(answer, _)
+    end
+
+    def test_a_input
+        answer = _("input.txt")
+        assert_equal(answer, _)
+    end
+
+    def test_b_sample
+        answer = _("sample.txt")
+        assert_equal(answer, _)
+    end
+
+    def test_b_input
+        answer = _("input.txt")
+        assert_equal(answer, _)
+    end
+end
