3ad243bc616f2db0cae3eaa0ccd61bd188674780
Chris Weathers <clweathers1@gmail.com>
2022-12-11 02:21:41 -0500

    Day 10 - part one complete.

--- 10/10.rb
+++ 10/10.rb
@@ -1,3 +1,44 @@
+def operation_for_line(line)
+    components = line.split
+    instruction = components.at(0).to_sym
+    value = components.at(1).to_i
+    cycles_to_complete = (instruction == :addx ? 2 : 1)
+    operation = {instruction: instruction, value: value, cycles_to_complete: cycles_to_complete}
+end
+
+def register_history(filename)
+    current_operations = []
+    register_value = 1
+    register_history = [register_value]
+
+    lines = File.readlines(filename, chomp: true)
+    lines.each do |line|
+        operation = operation_for_line(line)
+        current_operations << operation
+        current_operations.each_with_index do |operation, index|
+            cycles_to_complete = operation[:cycles_to_complete]
+            cycles_to_complete -= 1
+            operation[:cycles_to_complete] = cycles_to_complete
+
+            register_history << register_value
+
+            if cycles_to_complete == 0
+                value = operation[:value]
+                register_value += value
+            end
+
+            current_operations = current_operations.filter {|operation| operation[:cycles_to_complete] > 0}
+        end
+    end
+
+    return register_history
+end
+
 if __FILE__ == $0
-    
+    register_history = register_history("input.txt")
+    signal_strengths = register_history.map.with_index {|register_value, index| register_value * index}
+    interesting_signal_indices = [20, 60, 100, 140, 180, 220]
+    interesting_signal_strengths = interesting_signal_indices.map {|index| signal_strengths[index]}
+    interesting_signal_strengths_sum = interesting_signal_strengths.sum
+    puts interesting_signal_strengths_sum
 end
--- 10/10_test.rb
+++ 10/10_test.rb
@@ -2,23 +2,21 @@ require "test/unit"
 require_relative "10.rb"
 
 class TenTest < Test::Unit::TestCase
-    # def test_a_sample
-    #     answer = _("sample.txt")
-    #     assert_equal(answer, _)
-    # end
+    def test_sample_a
+        register_history = register_history("sample.txt")
+        signal_strengths = register_history.map.with_index {|register_value, index| register_value * index}
+        interesting_signal_indices = [20, 60, 100, 140, 180, 220]
+        interesting_signal_strengths = interesting_signal_indices.map {|index| signal_strengths[index]}
+        interesting_signal_strengths_sum = interesting_signal_strengths.sum
+        assert_equal(13140, interesting_signal_strengths_sum)
+    end
 
-    # def test_a_input
-    #     answer = _("input.txt")
-    #     assert_equal(answer, _)
-    # end
-
-    # def test_b_sample
-    #     answer = _("sample.txt")
-    #     assert_equal(answer, _)
-    # end
-
-    # def test_b_input
-    #     answer = _("input.txt")
-    #     assert_equal(answer, _)
-    # end
+    def test_input_a
+        register_history = register_history("input.txt")
+        signal_strengths = register_history.map.with_index {|register_value, index| register_value * index}
+        interesting_signal_indices = [20, 60, 100, 140, 180, 220]
+        interesting_signal_strengths = interesting_signal_indices.map {|index| signal_strengths[index]}
+        interesting_signal_strengths_sum = interesting_signal_strengths.sum
+        assert_equal(12560, interesting_signal_strengths_sum)
+    end
 end
