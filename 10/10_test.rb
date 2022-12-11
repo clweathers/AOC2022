5286eeaf7619a902aab6bd2dd339ceccc8228ce2
Chris Weathers <clweathers1@gmail.com>
2022-12-11 01:13:52 -0500

    Day 10 - initial commit.

---
+++ 10/10_test.rb
@@ -0,0 +1,24 @@
+require "test/unit"
+require_relative "10.rb"
+
+class TenTest < Test::Unit::TestCase
+    # def test_a_sample
+    #     answer = _("sample.txt")
+    #     assert_equal(answer, _)
+    # end
+
+    # def test_a_input
+    #     answer = _("input.txt")
+    #     assert_equal(answer, _)
+    # end
+
+    # def test_b_sample
+    #     answer = _("sample.txt")
+    #     assert_equal(answer, _)
+    # end
+
+    # def test_b_input
+    #     answer = _("input.txt")
+    #     assert_equal(answer, _)
+    # end
+end
